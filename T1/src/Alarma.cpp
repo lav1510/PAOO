c5979dc6dbfb883ae7385cca45ac4dcaea71451b
lav1510 <maria.bugarin@student.upt.ro>
2023-10-30 01:38:09 +0200

    Added alarm library

---
+++ T1/src/Alarma.cpp
@@ -0,0 +1,1 @@
+#include "Alarma.hpp"
---
+++ T1/src/Alarma.hpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <chrono>
+#include <iostream>
+
+enum AlarmSound {
+    BEEP,
+    BUZZER,
+    RADIO
+};
+
+class Alarm{
+    private:
+        int hour;
+        int minutes; //separate class(hour, minutes) in the future with proper bounds on members
+
+        char *note;
+
+        AlarmSound sound;
+
+        bool isAlarmSet;
+        bool activeOnWorkingDays;
+
+        bool activeSnooze;
+        int minutesSnooze;
+
+    public:
+        Alarm(int hour, int minutes, char *note, AlarmSound sound, bool isAlarmSet, bool activeOnWorkingDays, bool activeSnooze, int minutesSnooze);
+        Alarm(int hour, int minutes, char *note);
+        Alarm(int hour, int minutes);
+        Alarm(const Alarm &a);
+        ~Alarm();
+
+        void setHour          (int hour);
+        void setMinutes       (int minutes);
+        void setNote          (char *note);
+        void setSound         (AlarmSound sound);
+        void setAlarm         (bool isAlarmSet);
+        void setWorkingDays   (bool activeOnWorkingDays);
+        void setSnooze        (bool activeSnooze);
+        void setMinutesSnooze (int minutesSnooze);
+
+        int        getHour          ();
+        int        getMinutes       ();
+        char*      getNote          ();
+        AlarmSound getSound         ();
+        bool       getAlarm         ();
+        bool       getWorkingDays   ();
+        bool       getSnooze        ();
+        int        getMinutesSnooze ();
+
+        bool compare(Alarm a);
+
+        void display();
+};

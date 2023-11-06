ce329474394dcc8c28f74dc6f01ef71a2d49cbfa
lav1510 <maria.bugarin@student.upt.ro>
2023-11-07 01:21:07 +0200

    Made interface a .hpp file

--- T2/src/RingTimeDevice.cpp
+++
@@ -1,9 +0,0 @@
-class RingTimeDevice {
-    protected:
-    int hour;
-    int minutes;
-    
-    public:
-        virtual void setTime(int hour, int minutes);
-        virtual void ring();
-};
---
+++ T2/src/RingTimeDevice.hpp
@@ -0,0 +1,14 @@
+#pragma once
+class RingTimeDevice {
+    protected:
+    int hour;
+    int minutes;
+    
+    public:
+        virtual void setHour    (int hour)= 0;
+        virtual void setMinutes (int minutes) = 0; 
+        virtual int  getHour    () = 0; 
+        virtual int  getMinutes () = 0; 
+        virtual void setTime    (int hour, int minutes) = 0; 
+        virtual void ring       ()= 0; 
+};

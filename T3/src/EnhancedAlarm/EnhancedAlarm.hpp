47a9969337952db559e5aa9396ef0fec121d94cb
lav1510 <maria.bugarin@student.upt.ro>
2023-11-13 18:22:11 +0200

    Added namespace

--- T3/src/EnhancedAlarm/EnhancedAlarm.hpp
+++ T3/src/EnhancedAlarm/EnhancedAlarm.hpp
@@ -3,47 +3,49 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <memory>
 #include "../Alarm/Alarm.hpp"
 
-enum AlarmSound {
-    BEEP,
-    BUZZER,
-    RADIO
-};
+namespace EnhancedAlarms{
+    enum AlarmSound {
+        BEEP,
+        BUZZER,
+        RADIO
+    };
 
-template<typename T>
-class EnhancedAlarm : public Alarm{
-        T sound;
-        bool activeOnWorkingDays;
-        bool activeSnooze;
-        int minutesSnooze;
-        std::vector<int> activeDays; // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+    template<typename T>
+    class EnhancedAlarm : public Alarm{
+            T sound;
+            bool activeOnWorkingDays;
+            bool activeSnooze;
+            int minutesSnooze;
+            std::vector<int> activeDays; // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
 
-    public:
-        EnhancedAlarm();
-        EnhancedAlarm(int      hour, 
-            int        minutes, 
-            char       *note, 
-            T          sound, 
-            bool       isAlarmSet, 
-            bool       activeOnWorkingDays, 
-            bool       activeSnooze, 
-            int        minutesSnooze
-        );
-        ~EnhancedAlarm();
+        public:
+            EnhancedAlarm();
+            EnhancedAlarm(int      hour, 
+                int        minutes, 
+                char       *note, 
+                T          sound, 
+                bool       isAlarmSet, 
+                bool       activeOnWorkingDays, 
+                bool       activeSnooze, 
+                int        minutesSnooze
+            );
+            ~EnhancedAlarm();
 
-        void setWorkingDays   (bool activeOnWorkingDays);
-        void setSound         (const T& sound);
-        void setSnooze        (bool activeSnooze);
-        void setMinutesSnooze (int minutesSnooze);
+            void setWorkingDays   (bool activeOnWorkingDays);
+            void setSound         (const T& newSound);
+            void setSnooze        (bool activeSnooze);
+            void setMinutesSnooze (int minutesSnooze);
 
-        bool       getWorkingDays   (); 
-        T          getSound         ()const;
-        bool       getSnooze        ();
-        int        getMinutesSnooze ();
+            bool       getWorkingDays   (); 
+            bool       getSnooze        ();
+            int        getMinutesSnooze ();
 
-        void display();
+            void display();
 
-        void addActiveDay(int day);
-        bool isAlarmActive(int day);
-};
+            void addActiveDay(int day);
+            bool isAlarmActive(int day);
+    };
+}

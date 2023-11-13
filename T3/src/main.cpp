807d607c4bb53e41c993e32544ecb02d7f5db413
lav1510 <maria.bugarin@student.upt.ro>
2023-11-13 22:48:12 +0200

    Modified main with unique and shared pointers

--- T3/src/main.cpp
+++ T3/src/main.cpp
@@ -1,10 +1,11 @@
 #include "Alarm/Alarm.hpp"
+#include "AlarmList/AlarmList.hpp"
 #include "Timer/Timer.hpp"
 #include"EnhancedAlarm/EnhancedAlarm.hpp"
 #include"EnhancedAlarm/EnhancedAlarm.cpp"
 #include <iostream>
 
-template class EnhancedAlarms::EnhancedAlarm<int>;
+
 
 int main(int argc, char* argv[])
 {
@@ -16,9 +17,18 @@ int main(int argc, char* argv[])
     EnhancedAlarms::EnhancedAlarm<std::string> eh2;
     eh2.setSound("BUZZ");
     eh2.display();
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< std::endl;
 
-    Alarm alarm1(0,5);
-    alarm1.ring();
+    auto alarm1 = std::make_shared<Alarms::Alarm>(8, 0);
+    auto alarm2 = std::make_unique<Alarms::Alarm>(12, 30);
+    auto alarm3 = std::make_unique<Alarms::Alarm>(18, 0);
+
+    AlarmLists::AlarmList alarmList;
+
+    alarmList.addFirstAlarm(alarm1, alarm2);
+    alarmList.addAlarm(alarm3);
+
+    alarmList.displayAll();
 
     return 0;
 }
